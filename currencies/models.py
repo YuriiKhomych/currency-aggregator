c293c7e665853f65723e0827792df7bfc1e799f2
yuriikhomych <yuriykhomich@gmail.com>
2018-10-24 22:56:07 +0300

    Create CurrencyRate model with update_currency_rate method

--- currencies/models.py
+++ currencies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class CurrencyRate(models.Model):
+    base = models.CharField(max_length=10)
+    target = models.CharField(max_length=10)
+    price = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.base}-{self.target}-{self.price}'
+
+    def update_currency_rate(self, price):
+        if self.price != price:
+            self.price = price
+            self.save()
