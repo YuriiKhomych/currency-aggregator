9da77196c9541298bc8ca3fb0b27cdf228258ce6
yuriikhomych <yuriykhomich@gmail.com>
2018-10-24 22:58:05 +0300

    Create main views

--- currencies/views.py
+++ currencies/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView
 
-# Create your views here.
+from .forms import CurrencyRateForm
+from .models import CurrencyRate
+
+
+class CurrencyOnlineView(ListView):
+    queryset = CurrencyRate.objects.all()
+
+
+class CurrencyListView(ListView):
+    queryset = CurrencyRate.objects.all()
+
+
+class CurrencyRateListView(ListView):
+    queryset = CurrencyRate.objects.all()
+
+
+class CurrencyCreateView(CreateView):
+    form_class = CurrencyRateForm
+    queryset = CurrencyRate.objects.all()
+    success_url = '/currencies/rate/'
