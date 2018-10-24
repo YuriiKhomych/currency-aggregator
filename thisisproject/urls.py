0ee982abc6ed5b84785d2d61e4895fc78904c22d
yuriikhomych <yuriykhomich@gmail.com>
2018-10-24 23:01:37 +0300

    Include currencies app urls to project urls

--- thisisproject/urls.py
+++ thisisproject/urls.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("currencies/", include("currencies.urls"))
+]
